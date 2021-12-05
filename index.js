efc16fde3cdf99e882e6d517dc6480541bf06894
Lemuel <lscheon@oakland.edu>
2021-12-05 11:15:27 -0500

    changes to .gitignore

--- index.js
+++ index.js
@@ -95,5 +95,5 @@ app.get("/readanswers", (req, res) => {
 });
 
 // Setup server ports
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
